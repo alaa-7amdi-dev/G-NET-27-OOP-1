e0855839be399f805cb2cd322b7583768460c001
alaa-7amdi-dev <hamdihafezalaa@gmail.com>
2026-02-18 01:35:17 +0200

    Public Vs Private

--- Program.cs
+++ Program.cs
@@ -22,10 +22,30 @@
             //st2.StudentName = "memo";
             //Console.WriteLine(student.StudentName);
             //Console.WriteLine(st2.StudentName);
-            
-            
+
+
             //A class is a reference type that serves as a blueprint for creating objects
             //Reference type
+
+            #endregion
+            #region Public Vs Private
+
+            /*
+             private:The member is only accessible within the class or struct it is defined in.
+            It cannot be accessed from outside the class.
+            -- Use for members that should only be accessible within the class
+
+             private x;
+            private _db;
+             */
+
+            /*
+             public:he member is accessible from anywhere in the application.
+            --Use when you want to expose a class or method to other code.
+             
+            Public string Name { get; set; };
+
+             */
 
             #endregion
 
