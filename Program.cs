daf16d5993a5484e5d7d62075104a63a2d2c3fe9
alaa-7amdi-dev <hamdihafezalaa@gmail.com>
2026-02-18 01:27:23 +0200

    Class Vs Struct

--- Program.cs
+++ Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            #region Class Vs Struct
+            //Struct
+            //Point point = new Point();
+            //point.X = 10;
+            //point.Y = point.X;
+            //point.Y = 20;
+            //Console.WriteLine(point.X);   //=>10
+            //Console.WriteLine(point.Y);
+            //A struct is a value type used to represent small, lightweight data.
+            //Value type
+            //Class
+            //Student student = new Student(name: "ahmed", id: 1);
+            //Student st2 = student;
+            //st2.StudentName = "memo";
+            //Console.WriteLine(student.StudentName);
+            //Console.WriteLine(st2.StudentName);
+            
+            
+            //A class is a reference type that serves as a blueprint for creating objects
+            //Reference type
+
+            #endregion
+
+
+
         }
     }
 }
---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopAssignment1
+{
+    internal class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+
+        public Student( int id , string name)
+        {
+            StudentId = id;
+            StudentName = name;
+            
+        }
+    }
+}
